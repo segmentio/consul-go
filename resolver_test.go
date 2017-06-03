c6a5fa019082d2ee151eefad9f1f9559edbcf64e
Achille Roussel <achille@segment.com>
2017-06-02 17:03:04 -0700

    add consul.(*Resolver).LookupHost

--- resolver_test.go
+++ resolver_test.go
@@ -80,6 +80,67 @@ func testLookupService(t *testing.T, cache *ResolverCache) {
 	}
 }
 
+func testLookupHost(t *testing.T, cache *ResolverCache) {
+	server, client := newServerClient(func(res http.ResponseWriter, req *http.Request) {
+		if req.Method != "GET" {
+			t.Error("bad method:", req.Method)
+		}
+
+		if req.URL.Path != "/v1/health/service/1234" {
+			t.Error("bad URL path:", req.URL.Path)
+		}
+
+		foundQuery := req.URL.Query()
+		expectQuery := url.Values{
+			"passing":   {""},
+			"dc":        {"dc1"},
+			"tag":       {"A", "B", "C"},
+			"node-meta": {"answer:42"},
+		}
+		if !reflect.DeepEqual(foundQuery, expectQuery) {
+			t.Error("bad URL query:")
+			t.Logf("expected: %#v", expectQuery)
+			t.Logf("found:    %#v", foundQuery)
+		}
+
+		type service struct {
+			Address string
+			Port    int
+		}
+		json.NewEncoder(res).Encode([]struct {
+			Service service
+		}{
+			{Service: service{Address: "192.168.0.1", Port: 4242}},
+			{Service: service{Address: "192.168.0.2", Port: 4242}},
+			{Service: service{Address: "192.168.0.3", Port: 4242}},
+		})
+		return
+	})
+	defer server.Close()
+
+	rslv := Resolver{
+		Client:      client,
+		ServiceTags: []string{"A", "B", "C"},
+		NodeMeta:    map[string]string{"answer": "42"},
+		OnlyPassing: true,
+		Cache:       cache,
+	}
+
+	addrs, err := rslv.LookupHost(context.Background(), "1234")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !reflect.DeepEqual(addrs, []string{
+		"192.168.0.1:4242",
+		"192.168.0.2:4242",
+		"192.168.0.3:4242",
+	}) {
+		t.Error("bad addresses returned:", addrs)
+	}
+}
+
 func TestResolverCache(t *testing.T) {
 	list := []Endpoint{
 		{Addr: &serviceAddr{"192.168.0.1", 4242}},
